986969e43d97cde0e6c0dbd05f1720aee14cc582
Lyell Read <lyellread@gmail.com>
2019-11-11 15:39:36 -0800

    Adding some color sets

--- colortest.js
+++ colortest.js
@@ -5,10 +5,33 @@ const ansi = require('ansi');
 
 const cursor = ansi(process.stdout);
 
-//var reds = ['#ef9a9a', '#e57373', '#ef5350', '#f44336', '#e53935', '#FF3333', '#E11B1B', '#B32000', '#8E1F07'];
+//color sets with a colors related to <color>
+// var reds = ['#ef9a9a', '#ef5350', '#f44336', '#e53935', '#ff0a0a'];
+// var blues = [ '#87CCF1', '#00B9FF', '#47B2EB' , '#00A1F8', '#3C78C8'];
 
-var reds = ['#ffbebe', '#ffb2b2', '#ffa6a6', '#ff9a9a', '#ff8e8e', '#ff8282', '#ff7676', '#ff6a6a', '#ff5e5e', '#ff5252', '#ff4646', '#ff3a3a', '#ff2e2e', '#ff2222', '#ff1616', '#ff0a0a'];
+//color 'sequence' from white to <color>
+// var reds = ['#FFC9C9', '#FFA4A4', '#FF7575', '#FF3F3F', '#ff0a0a'];
+// var blues = [ '#D5EAFF', '#B0D8FF', '#75BAFF' , '#48A4FF', '#158BFF'];
+
+var greys = ['#FFFFFF', '#CECECE', '#A2A2A2', '#797979', '#464646'];
+
 reds.forEach(function(elem){
+	cursor.hex(elem);
+	process.stdout.write("█");
+	cursor.fg.reset();
+});
+
+console.log("");
+
+blues.forEach(function(elem){
+	cursor.hex(elem);
+	process.stdout.write("█");
+	cursor.fg.reset();
+});
+
+console.log("")
+
+greys.forEach(function(elem){
 	cursor.hex(elem);
 	process.stdout.write("█");
 	cursor.fg.reset();
